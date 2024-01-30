1bfe3cf07516b2606d363b07176005e1ad49ef9f
Celyne <dianakydd2@gmail.com>
2024-01-30 09:43:11 +0300

    Added the Github repo url and Netlify link

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by {""} <a href="https://github.com/mimi-netizen">Celyne Kydd</a>,
+        open-sourced on {""}
+        <a href="https://github.com/mimi-netizen/react-weather-app">
+          Github
+        </a>{" "}
+        and hosted on {""}{" "}
+        <a href="https://react-weather-project-codemom.netlify.app/">Netlify</a>
+      </footer>
+    </div>
+  );
 }
