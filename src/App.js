fec2a1d62c1e85fe31c0bb20977d702f2332f75c
Celyne <dianakydd2@gmail.com>
2024-01-30 09:57:30 +0300

    Added Bootsrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by {""} <a href="https://github.com/mimi-netizen">Celyne Kydd</a>,
-        open-sourced on {""}
-        <a href="https://github.com/mimi-netizen/react-weather-app">
-          Github
-        </a>{" "}
-        and hosted on {""}{" "}
-        <a href="https://react-weather-project-codemom.netlify.app/">Netlify</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by {""}{" "}
+          <a href="https://github.com/mimi-netizen">Celyne Kydd</a>,
+          open-sourced on {""}
+          <a href="https://github.com/mimi-netizen/react-weather-app">
+            Github
+          </a>{" "}
+          and hosted on {""}{" "}
+          <a href="https://react-weather-project-codemom.netlify.app/">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
