304961d215e3646e54803c4f28adfd09910d592a
Celyne <dianakydd2@gmail.com>
2024-01-30 10:43:43 +0300

    Added more Bootstrap styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>Nairobi</h1>
       <ul>
         <li>Wednesday 07:00am</li>
